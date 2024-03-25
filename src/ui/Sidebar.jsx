7a7c5e03f87a6ece90eba2a1f633b92bf918b1f9
Kevin Lami <kevinlami15@gmail.com>
2024-03-25 12:34:24 -0300

    feat: bookings, authentication and darkmode

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyleSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -17,6 +18,8 @@ function Sidebar() {
     <StyleSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyleSidebar>
   );
 }
